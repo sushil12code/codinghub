0bd703322f749de2fd7b2be4ab681f47dc11286d
sushil12code <sushilmahara5050@gmail.com>
2024-08-29 22:45:50 +0545

    html

---
+++ My Projects/script.js
@@ -0,0 +1,8 @@
+function downloadPhoto(url, filename) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
